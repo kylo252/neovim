7223add7ba2b3c03fb0b5e1906e0419d744394d0
kylo252 <59826753+kylo252@users.noreply.github.com>
2022-06-10 15:55:24 +0200

    test: better suite detection

--- cmake/LuaBusted.cmake
+++ cmake/LuaBusted.cmake
@@ -12,7 +12,7 @@ function(busted_discover_tests TARGET)
     ""
     ""
     "BUSTED_PRG;LUA_PRG;TEST_PREFIX;TEST_SUFFIX;WORKING_DIRECTORY;BUILD_DIR;TEST_LIST"
-    "EXTRA_ARGS;PROPERTIES"
+    "EXTRA_ARGS;TEST_ENVIRONMENT;PROPERTIES"
     ${ARGN}
   )
 
@@ -24,27 +24,30 @@ function(busted_discover_tests TARGET)
   endif()
 
   # TODO(kylo252): figure out if we can use a generator expression instead
-  get_target_property(SPEC_FILES ${TARGET} SOURCES)
+  get_target_property(SPECS ${TARGET} SOURCES)
 
-  foreach(SPEC_FILE ${SPEC_FILES})
-    get_filename_component(ctest_file_base ${SPEC_FILE} NAME_WLE CACHE)
+  foreach(SPEC_ROOT ${SPECS})
+    if(NOT IS_DIRECTORY "${SPEC_ROOT}")
+      get_filename_component(ctest_file_base ${SPEC_ROOT} NAME_WLE)
+    else()
+      file(RELATIVE_PATH ctest_file_base "${_WORKING_DIRECTORY}/test" "${SPEC_ROOT}")
+    endif()
     set(ctest_include_file "${CMAKE_CURRENT_BINARY_DIR}/${ctest_file_base}_include.cmake")
     set(ctest_tests_file "${CMAKE_CURRENT_BINARY_DIR}/${ctest_file_base}_tests.cmake")
 
-    # message("got ${ctest_file_base} ${ctest_file_dir} ${spec_file_relative}")
-
     add_custom_command(
-      TARGET ${TARGET} PRE_BUILD
+      TARGET ${TARGET} POST_BUILD
       BYPRODUCTS "${ctest_tests_file}"
       COMMAND "${CMAKE_COMMAND}"
       -D "TEST_TARGET=${TARGET}"
       -D "BUSTED_PRG=${_BUSTED_PRG}"
-      -D "SPEC_FILE=${SPEC_FILE}"
+      -D "SPEC_ROOT=${SPEC_ROOT}"
       -D "LUA_PRG=${_LUA_PRG}"
       -D "WORKING_DIR=${_WORKING_DIRECTORY}"
       -D "BUILD_DIR=${_BUILD_DIR}"
       -D "TEST_EXTRA_ARGS=${_EXTRA_ARGS}"
       -D "TEST_PROPERTIES=${_PROPERTIES}"
+      -D "TEST_ENVIRONMENT=${_TEST_ENVIRONMENT}"
       -D "TEST_PREFIX=${_TEST_PREFIX}"
       -D "TEST_SUFFIX=${_TEST_SUFFIX}"
       -D "TEST_LIST=${_TEST_LIST}"
@@ -62,27 +65,11 @@ function(busted_discover_tests TARGET)
       "endif()\n"
     )
 
-    if(NOT ${CMAKE_VERSION} VERSION_LESS "3.10.0")
-      # Add discovered tests to directory TEST_INCLUDE_FILES
-      set_property(
-        DIRECTORY
-        APPEND PROPERTY TEST_INCLUDE_FILES "${ctest_include_file}"
-      )
-    else()
-      # Add discovered tests as directory TEST_INCLUDE_FILE if possible
-      get_property(test_include_file_set DIRECTORY PROPERTY TEST_INCLUDE_FILE SET)
-      if(NOT ${test_include_file_set})
-        set_property(
-          DIRECTORY
-          PROPERTY TEST_INCLUDE_FILE "${ctest_include_file}"
-        )
-      else()
-        message(
-          FATAL_ERROR
-          "Cannot set more than one TEST_INCLUDE_FILE"
-        )
-      endif()
-    endif()
+    # Add discovered tests to directory TEST_INCLUDE_FILES
+    set_property(
+      DIRECTORY
+      APPEND PROPERTY TEST_INCLUDE_FILES "${ctest_include_file}"
+    )
   endforeach()
 
 endfunction()
